01b0316808d36112f62d9cd69efe0dedce0746d2
zhangyu <zhangyu34@gmail.com>
2014-10-06 19:35:07 +0800

    paixu

--- app/controllers/pingces_controller.rb
+++ app/controllers/pingces_controller.rb
@@ -1,10 +1,10 @@
 class PingcesController < ApplicationController
   def index
-    @pingces = Pingce.all.order("jielun asc")
+    @pingces = Pingce.all.order("jielun asc, c_at desc")
   end
 
   def author
-    @pingces = Pingce.where(:editor_id => params[:id]).order("jielun asc") 
+    @pingces = Pingce.where(:editor_id => params[:id]).order("jielun asc")
     render :action => :index
   end
 end
