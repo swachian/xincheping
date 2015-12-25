60f944966ad72492b6500f0b4fb4528ee61f1046
swachian <zhangyu34@gmail.com>
2015-12-25 10:29:39 +0800

    老版交通局监控点数据下载完毕，升级到4.2.5

--- app/models/electronic_police.rb
+++ app/models/electronic_police.rb
@@ -1,15 +1,17 @@
 class ElectronicPolice < ActiveRecord::Base
+  validates_uniqueness_of :position
+
   def self.fetch_one_page(page)
     puts page
     doc = Nokogiri::HTML(open(page), nil, "GBK")
     tds = doc.css("table table table table table table tr td:nth-child(2)")
     tds[1..-1].each do |td|
-      puts td.text
+      ElectronicPolice.create(position: td.children[1].text)
     end
   end
 
   def self.test_fetch_one_page()
-    1.upto(8) do |i|
+    1.upto(298) do |i|
     page = "http://www.shjtaq.com/zwfg/dzjc_address.asp?key=&page=#{i}"
     fetch_one_page(page)
     end
