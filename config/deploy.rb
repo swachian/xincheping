ecbf022f2d5da39ef7c2f7d2536b6f79a09b935d
zhangyu <zhangyu34@gmail.com>
2016-02-05 17:09:27 +0800

    racer

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/opt/deploy/xincheping'
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-set :default_env, { path: "~/.rbenv/versions/ruby-2.3/bin:$PATH" }
+set :default_env, { path: "/home/zhangyu/.rbenv/versions/ruby-2.3/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
